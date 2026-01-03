3237d44b89ff62beedf6acf0967870bfc3f86e03
AdityaGithub006 <aditya006it@gmail.com>
2026-01-03 11:38:00 -0500

    Fix CI: install Chrome + headless toggle

--- src/test/java/com/aditya/framework/driver/DriverFactory.java
+++ src/test/java/com/aditya/framework/driver/DriverFactory.java
@@ -25,30 +25,17 @@ public class DriverFactory {
                 throw new RuntimeException("Only chrome supported right now. Found: " + browser);
             }
 
-            ChromeOptions chromeOptions = new ChromeOptions();
+            ChromeOptions options = new ChromeOptions();
             // Use a clean session each run (avoid your real Chrome profile settings/popups)
-            chromeOptions.addArguments("--incognito");
-            if (headless) {
-                chromeOptions.addArguments("--headless=new");
-            }
-            driver.set(new ChromeDriver(chromeOptions));
+            options.addArguments("--incognito");
+
+            // Required for GitHub Actions / Linux
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+            driver.set(new ChromeDriver(options));
             driver.get().manage().window().maximize();
-
-            // Block password manager + related UI prompts             // Turn OFF password manager + credentials service
-//            Map<String, Object> prefs = new HashMap<>();
-//            prefs.put("credentials_enable_service", false);
-//            prefs.put("profile.password_manager_enabled", false);
-
-            // Optional: reduce other “Chrome automation” distractions
-//            prefs.put("profile.default_content_setting_values.notifications", 2);
-
-//            chromeOptions.setExperimentalOption("prefs", prefs);
-
-            // Disable password leak detection feature (this popup)
-//            chromeOptions.addArguments("--disable-features=PasswordLeakDetection,AutofillPasswordLeakDetection");
-
-//            chromeOptions.addArguments("--user-data-dir=target/chrome-profile");
-
         }
     }
     public void quit(){
