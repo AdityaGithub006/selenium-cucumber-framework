7a30f55a962241bf6b32a6c5984be7b47c2a7943
AdityaGithub006 <aditya006it@gmail.com>
2026-01-03 11:42:39 -0500

    Fix CI: install Chrome + headless toggle

--- src/test/java/com/aditya/framework/driver/DriverFactory.java
+++ src/test/java/com/aditya/framework/driver/DriverFactory.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class DriverFactory {
     ThreadLocal<WebDriver> driver = new ThreadLocal<>();
     private final ConfigReader config;
-
     public DriverFactory(ConfigReader config) {
         this.config = config;
     }
@@ -19,21 +18,25 @@ public class DriverFactory {
     public void start(){
         if (driver.get() == null){
             String browser = config.get("browser");
-            Boolean headless = config.getBool("headless");
+//            Boolean headless = config.getBool("headless");
 
             if (!browser.equalsIgnoreCase("chrome")) {
                 throw new RuntimeException("Only chrome supported right now. Found: " + browser);
             }
+            boolean headless = Boolean.parseBoolean(System.getProperty("headless",
+                    String.valueOf(config.getBool("headless"))));
 
             ChromeOptions options = new ChromeOptions();
             // Use a clean session each run (avoid your real Chrome profile settings/popups)
             options.addArguments("--incognito");
 
-            // Required for GitHub Actions / Linux
-            options.addArguments("--headless=new");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--window-size=1920,1080");
+            // CI-safe flags
+            if (headless) {
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--window-size=1920,1080");
+            }
             driver.set(new ChromeDriver(options));
             driver.get().manage().window().maximize();
         }
